b21772bc062060fc62bddcd38fa66a0a8c0dffbd
Thomas Ferrari <thomasferrari7@gmail.com>
2023-08-30 17:37:02 +0200

    El proyecto ya puede pedir al usuario los dias y horas trabajadas y con esos datos calcular cuantas horas trabajo en el mes. Utilizando funcion, metodos, constructor, for of, condicionales, arrays y objetos

--- salario2.js
+++ salario2.js
@@ -1,49 +1,65 @@
 //----------CALCULADOR DE SALARIO------------
-//
+
+
 //Mensaje de bienvenida.
-alert("Bienvenido/a. \n\n Aquí podras calcular y llevar el control de tu salario! \n\n Empecemos!")
+alert("Bienvenido/a. \n\n Aquí podras calcular y llevar el control de tu salario! \n\n Empecemos!");
 
 //Se crea la funcion constructora Dia, para guardar cada dia trabajado y las horas trabajadas 
 class Dia {
     constructor (fecha, horasDiarias)  {
         this.fecha = fecha;
         this.horasDiarias = horasDiarias;
-    }
+    } 
 }
 
-
-//Creo arrays de DiasTabajados y HorasTrabajadas para guardar todo el mes 
+//Creo array de DiasTabajados para guardar todo el mes 
 const diasTrabajados = [];
-const horasTrabajadas = [];
 
 function nuevaEntrada() { //Creo funcion para abstraccion.
     const fecha = prompt("Ingrese fecha");
     const horasDiarias = parseFloat(prompt("Ingrese horas trabajadas"));
-    const nuevoDia = new Dia (fecha, horasDiarias);//Creo un nuevo dia con los datos que pedi antes.
     alert("Entrada cargada")
-    horasTrabajadas.push(horasDiarias) //Envio las horasDiarias a el array horasTrabajadas
-    diasTrabajados.push(nuevoDia) //Envio ese objeto (NuevoDia) a  diasTrabajados
-    pregunta = prompt("Deseas agregar nueva entrada?")
+    return new Dia (fecha, horasDiarias);//Creo un nuevo dia con los datos que pedi antes.
 }
-//----INGRESO DE DATOS------
-let pregunta = prompt("Deseas agregar nueva entrada?") //Pregunto al usuario si quiere agregar un nuevo dia trabajado
-while (pregunta === "SI" || pregunta === "Si" || pregunta === "si") {
+
+
+//------------INGRESO DE DATOS----------
+
+const pregunta = prompt("Deseas agregar nueva entrada? (Responder si o no)")//Pregunto al usuario si quiere agregar un nuevo dia trabajado, si responde que si, vuelve a comenzar el while.
+if (pregunta === "Si" || pregunta === "SI" || pregunta === "si") {
     
+    let agregarNuevaEntrada = true 
+    while (agregarNuevaEntrada) {
+        
+        const nuevoDia = nuevaEntrada(); //Creo nuevoDia, donde ejecuto la funcion que crea new Dia. 
+        /* nuevoDia.horasDiarias; */ //Asi puedo sumar las horas
+        
+        
+        diasTrabajados.push(nuevoDia); //Envio ese objeto (NuevoDia) al array diasTrabajados
+        
+        const pregunta = prompt("Deseas agregar nueva entrada?")//Pregunto al usuario si quiere agregar un nuevo dia trabajado, si responde que si, vuelve a comenzar el while.
+        if (pregunta === "Si" || pregunta === "SI" || pregunta === "si") { //Si responde que si, vuelve a comenzar el while.
+            agregarNuevaEntrada = true;
+        } else { // Si responde diferente a si termina el while.
+            agregarNuevaEntrada = false;
+            
+        }
+        
+    }
 } 
 
-let totalHoras = 0;
+//--------------PROCESAMIENTO DE DATOS--------------
+let totalHorasMes = 0;
+const horasTrabajadas = diasTrabajados.map(h => h.horasDiarias) //Creo horasTrabajadas, le digo que tome cada valor de horasDiareias dentro de diasTrabajados, y crea un array con estos valores.
 
 
-for (const i of horasTrabajadas) {
-    totalHoras = totalHoras + i
+for (const i of horasTrabajadas) { //Para cada indice de horasTrabajas 
+    totalHorasMes = totalHorasMes + i //Sumo ese indice al totalHorasMes.
 }
 
-function sumarHoras ()
+
+//-----------SALIDA DE DATOS----------- 
+
+alert("Este mes has trabajado " + totalHorasMes + " horas!")//Muestro en alert la suma de horas trabajadas en el mes.
 
 
-
-alert("Este mes has trabajado " + totalHoras + " horas!")
-
-
-/* console.log(diasTrabajados);
-console.log(horasTrabajadas) */
