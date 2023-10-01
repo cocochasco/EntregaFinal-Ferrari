adf73d0c1c0424f0b6f6de3442a1ab14f263cc9b
Thomas Ferrari <thomasferrari7@gmail.com>
2023-10-02 01:17:17 +0200

    Proyecto terminado

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 // DOM - ENTRADA DE DATOS
-const registroHoras = document.querySelector('#registroHoras') //
+const registroHoras = document.querySelector('#registroHoras') 
 const btnConfirmarPrecioHora = document.querySelector('#confirmarPrecioHora')
-const inputFecha = document.querySelector('#inputFecha')//
-const inputHoraEntrada = document.querySelector('#inputHoraEntrada')//
-const inputHoraSalida = document.querySelector('#inputHoraSalida')//
-const formInformacion = document.querySelector('#formInformacion') //
+const inputFecha = document.querySelector('#inputFecha')
+const inputHoraEntrada = document.querySelector('#inputHoraEntrada')
+const inputHoraSalida = document.querySelector('#inputHoraSalida')
+const divPrecioHora = document.querySelector('#divPrecioHora') 
 const btnModificarPrecioHora = document.querySelector('#btnModificarPrecioHora')
 
 //DOM - SALIDA DE DATOS
 const horasMensuales = document.querySelector('#horasMensuales')
-/* const resumenMensualFecha = document.querySelector('#resumenMensualFecha')
-const resumenMensualHoras = document.querySelector('#resumenMensualHoras') */
 const salarioMensual = document.querySelector('#salarioMensual')
 const pHorasSinCargar = document.querySelector('#pHorasSinCargar')
 const pSalarioSinCargar = document.querySelector('#pSalarioSinCargar')
 const tablaResumen = document.querySelector('#tablaResumen')
 const tBody = document.querySelector('#tBody')
+const main = document.querySelector('#main')
 
-//Creo constante diasTrabajados, donde le digo que si ya tiene informacion en el localStorage, la parsee y la utilice, y sino que sea un array vacio
-const diasTrabajados = JSON.parse(localStorage.getItem('diasTrabajados')) || [];
-
-//Creo clase Dia, para guardar cada dia trabajado con su fecha y horasDiarias
 class Dia {
     constructor ({fecha, horasDiarias})  {
         this.fecha = fecha;
@@ -28,16 +23,15 @@ class Dia {
     } 
 } 
 
-//Creo variable submit para saber si ya se ha guardado alguna informacion o no, y segun esto aplicar diferentes funciones luego.
+const diasTrabajados = JSON.parse(localStorage.getItem('diasTrabajados')) || [];
+
 let submit = JSON.parse(localStorage.getItem('submit')) || 0;
 
+
+//------EVENTOS------
 btnConfirmarPrecioHora.addEventListener ('click', e => {
     e.preventDefault()
     
-    sumarSubmit() 
-    if (submit === 1) {
-        mostrarPrecioFijado() //Para que mostrarPrecioFijado se ejecute solo la primera vez que envio el formulario. Sino se ejecuta siempre y tira un error porque no puede removerChild de un elemento que ya no existe
-    }
     confirmarPrecioHora()
     mostrarPrecioFijado()
     if(diasTrabajados.length > 0){
@@ -51,53 +45,47 @@ btnModificarPrecioHora.addEventListener ('click', e => {
     modificarPrecioHora()
 })
 
-
-//Creo evento para el submit
 registroHoras.addEventListener ('submit', e => {
     e.preventDefault()
-    const fecha = inputFecha.value //Transformo valores de datos ingresados en HTML en variables
+    
+    const fecha = inputFecha.value 
     const horaEntrada = inputHoraEntrada.value
     const horaSalida = inputHoraSalida.value
     
-    
     const horasDiarias = parseFloat(calcularHorasDiarias(horaEntrada, horaSalida))
-    
-    
-    const nuevoDia = new Dia({ fecha, horasDiarias }) //Utilizo el valor de esas variables para crear nuevo Dia
-    
-    // EJECUCION DE FUNCIONES DENTRO DEL EVENTO
+    const nuevoDia = new Dia({ fecha, horasDiarias }) 
+   
     if ( horaSalida >  horaEntrada){
-    cargarNuevoDia(nuevoDia)
-    
-    ordenarResumen()
-    
-    sumarHorasTrabajadas()
-    
-    actualizarInformacion()
-    tablaResumen.style.display = 'block'
-    
-    inputFecha.value='' //Reseteo valores a 0
-    inputHoraEntrada.value=''
-    inputHoraSalida.value=''
-}else {
-    alertarError()
-}
-}
-) 
+        sumarSubmit() 
+        cargarNuevoDia(nuevoDia)
+        ordenarResumen()
+        sumarHorasTrabajadas()
+        actualizarInformacion()
+
+        inputFecha.value='' 
+        inputHoraEntrada.value=''
+        inputHoraSalida.value=''
+    }else {
+        alertarError()
+    }
+}) 
+
 
 //-----EJECUCION DE FUNCIONES-----
 
 mostrarPrecioFijado()
+mostrarResumenMensual()
 
-if(diasTrabajados.length > 0 ) { //Porque si dias trabajados esta vacio, estaria intentando ejecutar una funcion trabajando sobre un array vacio, y me tira error
+if(diasTrabajados.length > 0 ) { 
     ordenarResumen()
     actualizarInformacion()
 }
 
+
 //-----FUNCIONES-----
 
 function confirmarPrecioHora() {
-    const inputPrecioHora = document.querySelector('#inputPrecioHora')//
+    const inputPrecioHora = document.querySelector('#inputPrecioHora')
     const precioHora = parseFloat(inputPrecioHora.value)
     localStorage.setItem('precioHora', JSON.stringify(precioHora));
 
@@ -105,15 +93,14 @@ function confirmarPrecioHora() {
 }
 
 function modificarPrecioHora() {
-    const inputPrecioHora = document.querySelector('#ninputPrecioHora')  //
-    formInformacion.removeChild(inputPrecioHora)
+    const inputPrecioHora = document.querySelector('#parrafoPrecioHora') 
+    divPrecioHora.removeChild(inputPrecioHora)
 
     const nuevoHoras = document.createElement('input')
     nuevoHoras.type = 'number'
     nuevoHoras.id = 'inputPrecioHora'
 
-    formInformacion.appendChild(nuevoHoras)
-
+    divPrecioHora.appendChild(nuevoHoras)
 }
 
 function calcularHorasDiarias(horaEntrada, horaSalida) {
@@ -151,10 +138,9 @@ function ordenarResumen() {
     diasTrabajados.sort(compararFechas)
 }
 
-function compararFechas(a, b) { //FUNCION DE COMPARACION: Comparo las fechas dentro de los objetos dentro del array, para despues poder aplciar el .sort
+function compararFechas(a, b) { 
   const fechaA = a.fecha;
   const fechaB = b.fecha;
-
   if (fechaA < fechaB) {
     return -1;
   }
@@ -166,21 +152,18 @@ function compararFechas(a, b) { //FUNCION DE COMPARACION: Comparo las fechas den
 
 function sumarHorasTrabajadas() {
         const horasTrabajadas = diasTrabajados.map(h => h.horasDiarias).reduce((a,b) => a+b) 
-        console.log(horasTrabajadas)
         return horasTrabajadas
 }
 
-
 function mostrarHorasTrabajadas() {
-        const horasTrabajadas = sumarHorasTrabajadas()
-        const dias = diasTrabajados.length
-        if (submit===0){
-            horasMensuales.removeChild(pHorasSinCargar)
-        }else {
-            horasMensuales.innerHTML =`${horasTrabajadas} horas y ${dias} dias!`
-        }
+    const horasTrabajadas = sumarHorasTrabajadas()
+    const dias = diasTrabajados.length
+    if (submit===0){
+        horasMensuales.removeChild(pHorasSinCargar)
+    }else {
+        horasMensuales.innerHTML =`${horasTrabajadas} horas y ${dias} dias!`
+    }
 }
-
 
 function mostrarSalarioMensual() {
     const horasTrabajadas = sumarHorasTrabajadas()
@@ -194,42 +177,27 @@ function mostrarSalarioMensual() {
 }
 
 function mostrarResumenMensual() {
-
+    if ( diasTrabajados.length > 0) {
         tBody.innerHTML = ''
+        for (let i = 0; i < diasTrabajados.length; i++) {
+            const resumen = document.createElement('tr')
+            tBody.appendChild(resumen)
 
-        const resumenFecha = diasTrabajados.map(d => d.fecha)
-        const resumenHoras = diasTrabajados.map(d => d.horasDiarias)
-        
-        const resumen = document.createElement('tr')
-        
-        for (const fecha of resumenFecha) {
             const itemFecha = document.createElement('td')
-            itemFecha.innerHTML = fecha
+            itemFecha.innerHTML = diasTrabajados[i].fecha
             resumen.appendChild(itemFecha)
 
-
-            resumen.className = 'lista'//Creo clase para darle estilo en css
-            itemFecha.className = 'item'
-          
-        }
-    
-        
-
-       
-        
-        for (const horas of resumenHoras) {
-        
-           
             const itemHoras = document.createElement('td')
-            itemHoras.innerHTML = horas
+            itemHoras.innerHTML = diasTrabajados[i].horasDiarias
             resumen.appendChild(itemHoras)
-            resumen.className = 'lista'
-            itemHoras.className = 'item'
-            
+
+            resumen.className = 'tr'
+            itemHoras.className = 'td'
+            itemFecha.className = 'td'
         }
-        
-        tBody.style.display = 'block'
-        
+    } else {
+        main.removeChild(tablaResumen)
+    }
 } 
 
 function actualizarInformacion() {
@@ -242,24 +210,10 @@ function mostrarPrecioFijado() {
      if (JSON.parse(localStorage.getItem('precioHora'))) {
         const inputPrecioHora = document.querySelector('#inputPrecioHora')//
         const precioHora = JSON.parse(localStorage.getItem('precioHora'))|| parseFloat(inputPrecioHora.value)
-        formInformacion.removeChild(inputPrecioHora)
-        const ninputPrecioHora = document.createElement('p')
-        ninputPrecioHora.innerHTML = `${precioHora}`
-        ninputPrecioHora.id = 'ninputPrecioHora'
-        formInformacion.appendChild(ninputPrecioHora)
+        divPrecioHora.removeChild(inputPrecioHora)
+        const parrafoPrecioHora = document.createElement('p')
+        parrafoPrecioHora.innerHTML = `${precioHora}`
+        parrafoPrecioHora.id = 'parrafoPrecioHora'
+        divPrecioHora.appendChild(parrafoPrecioHora)
      }
 }
-
-
-
-/*?????????????
-
--const horasTrabajadas = sumarHorasTrabajadas()// PORQUE NO LOGRO DECLARARLO AFUERA DE LAS FUNCIONES Y TENGO QUE DECLARARLO DENTRO DE CADA FUNCION QUE QUIERO APLICAR??
-
--Como hacer que presionando un boton aparezca el formulario de un nuevo dia?
-
-Solucionar el ok cuando no modifique las horas...
-
-Que resumen mensual se vea solo cuando cargo info y que sea tabla
-
-??????????*/
